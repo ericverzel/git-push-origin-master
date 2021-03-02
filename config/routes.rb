3233e8a42580b93bfb9a536621174eaf399d8fc6
cd ~/code/$ericverzel/dotfiles && zsh git_setup.sh <ericziouane@gmail.com>
2021-03-02 15:46:15 +0100

    adddind CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlrails
+  #show all tasks
+  get "tasks" => "tasks#index"
+  #show one task
+  get "tasks/:id" => "tasks#show"
+  #create a task
+  get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+  #update task
+  get "tasks/:id/edit" => "tasks#edit"
+  patch "tasks/:id" => "tasks#update"
+  #delete task
+  delete "tasks/:id" => "tasks#destroy"
 end
